ee52fd59f9ba2a933bf1ec8f267b970842a266d4
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-05-14 22:46:41 +0200

    Added fish spawner

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.right * speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/FishSpawner.cs
+++ Assets/Scripts/FishSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
@@ -5,20 +6,23 @@ public class FishSpawner : MonoBehaviour
     public GameObject[] Fishes;
     private int randomFish;
     private Vector3 spawnPosition;
+    private int spawnVertical;
     private Quaternion spawnRotation;
     public Camera mainCamera;
+    public int spawnDelay;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        randomFish = Random.Range(0, Fishes.Length);
-        spawnPosition = mainCamera.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, 2));
-        Instantiate(Fishes[randomFish], spawnPosition, spawnRotation);
+        InvokeRepeating("SpawnFish", 2, spawnDelay);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnFish()
     {
-
+        randomFish = Random.Range(0, Fishes.Length);
+        spawnVertical = Random.Range(0, Screen.height / 2);
+        float spawnHorizontal = Random.value < 0.5 ? -20 : Screen.width + 20;
+        spawnRotation = spawnHorizontal == -20 ? Quaternion.Euler(0, 180, 180) : Quaternion.Euler(0, 180, 0);
+        spawnPosition = mainCamera.ScreenToWorldPoint(new Vector3(spawnHorizontal, (Screen.height / 2) - spawnVertical, 2));
+        Instantiate(Fishes[randomFish], spawnPosition, spawnRotation);
     }
 }
