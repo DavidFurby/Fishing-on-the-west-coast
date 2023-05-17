9f33c6b489dc287b26962c2383044f31e3abea16
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-05-17 13:36:29 +0200

    Added Catch area to bait

--- Assets/Scripts/CatchArea.cs
+++ Assets/Scripts/CatchArea.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bait : MonoBehaviour
+public class CatchArea : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Fish"))
         {
-            other.GetComponent<FishMovement>().GetBaited(transform.position);
+            Debug.Log("Get it!");
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                Debug.Log("Got it!");
+            }
         }
     }
 }
--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
-    [SerializeField] float speed;
     [SerializeField] float swimSpeed;
     public Vector3 direction;
     private Rigidbody rb;
@@ -12,26 +11,17 @@ public class FishMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         InvokeRepeating(nameof(Swim), 1, 4);
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-    }
-    // Method to move fish based on speed and direction
-    void FloatSpeed()
-    {
-        transform.position += direction * speed * Time.fixedDeltaTime;
-    }
     // Coroutine to add force to fish's rigidbody
     void Swim()
     {
-        rb.AddForce(direction * swimSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+        rb.AddForce(swimSpeed * Time.fixedDeltaTime * direction, ForceMode.Impulse);
     }
     //Move towards bait if triggered
     public void GetBaited(Vector3 baitPosition)
     {
+        Debug.Log(baitPosition);
         direction = (baitPosition - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 
 }
