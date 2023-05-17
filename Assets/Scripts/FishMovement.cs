f26551aa8bd8218dcdc71f393e750ce5eef9fafa
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-05-17 12:38:11 +0200

    Fixed the swim method

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -1,26 +1,22 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class FishMovement : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float swimSpeed;
     public Vector3 direction;
-    [SerializeField] BoxCollider sea;
-    private float destroyTimer = 10;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        InvokeRepeating(nameof(Swim), swimSpeed, swimSpeed);
+        InvokeRepeating(nameof(Swim), 1, 4);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        FloatSpeed();
+
     }
     // Method to move fish based on speed and direction
     void FloatSpeed()
