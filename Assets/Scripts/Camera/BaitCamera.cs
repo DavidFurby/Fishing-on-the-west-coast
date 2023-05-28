faaa6fc892ad2d60acc4c0d0d1c84ebe6824b0b5
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-05-28 16:47:05 +0200

    Updated environment home exterior

--- Assets/Scripts/Camera/BaitCamera.cs
+++ Assets/Scripts/Camera/BaitCamera.cs
@@ -6,15 +6,20 @@ public class BaitCamera : MonoBehaviour
 {
     [SerializeField] FishingControlls fishingControlls;
     [SerializeField] GameObject bait;
-    private float cameraDistance = -6.0f;
+    private float cameraDistance;
 
+    private void Start()
+    {
+        cameraDistance = transform.position.z;
+    }
     // Update is called once per frame
     void LateUpdate()
     {
         if (fishingControlls.fishingStatus != FishingControlls.GetFishingStatus.StandBy)
         {
+
             transform.position = new Vector3(bait.transform.position.x, bait.transform.position.y, cameraDistance);
-            if (cameraDistance < 0)
+            if (cameraDistance < bait.transform.position.z - 2)
             {
                 cameraDistance += 0.1f;
             }
--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -5,6 +5,6 @@ public class PlayerCamera : MonoBehaviour
     public GameObject player;
     private void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, 3, -6);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, -4);
     }
 }
