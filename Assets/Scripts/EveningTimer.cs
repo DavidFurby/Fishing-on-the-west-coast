14e5c2830383e5b1082808ed72533ff98c757acd
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-05-29 21:23:22 +0200

    Fixed evening timer

--- Assets/Scripts/EveningTimer.cs
+++ Assets/Scripts/EveningTimer.cs
@@ -4,12 +4,11 @@ public class EveningTimer : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] TransitArea transitArea;
-    [SerializeField] MainManager mainManager;
 
     public void EndDay()
     {
         Debug.Log("End day");
-        mainManager.game.days++;
+        MainManager.Instance.game.days++;
         transitArea.Transition();
     }
 }
