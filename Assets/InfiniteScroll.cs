6013e9bc89eae381a65b77e9bf66592ec1f44585
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-09-13 12:29:26 +0200

    Fixed the vertical scroll

--- Assets/InfiniteScroll.cs
+++ Assets/InfiniteScroll.cs
@@ -3,42 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InfiniteScroll : MonoBehaviour
+public class InfiniteScrollVertical : MonoBehaviour
 {
     public ScrollRect scrollRect;
     public RectTransform viewPortTransform;
     public RectTransform contentPanelTransform;
-    public HorizontalLayoutGroup HLG;
-    public RectTransform[] itemList;
+    public VerticalLayoutGroup VLG;
+
+    public RectTransform[] ItemList;
 
     Vector2 OldVelocity;
     bool isUpdated;
-
     // Start is called before the first frame update
     void Start()
     {
         isUpdated = false;
         OldVelocity = Vector2.zero;
-        int ItemsToAdd = Mathf.CeilToInt(viewPortTransform.rect.width / (itemList[0].rect.width + HLG.spacing));
+        int ItemsToAdd = Mathf.CeilToInt(viewPortTransform.rect.height / (ItemList[0].rect.height + VLG.spacing));
+
         for (int i = 0; i < ItemsToAdd; i++)
         {
-            RectTransform RT = Instantiate(itemList[i % itemList.Length], contentPanelTransform);
+            RectTransform RT = Instantiate(ItemList[i % ItemList.Length], contentPanelTransform);
             RT.SetAsLastSibling();
         }
+
         for (int i = 0; i < ItemsToAdd; i++)
         {
-            int num = itemList.Length - i - 1;
+            int num = ItemList.Length - i - 1;
             while (num < 0)
             {
-                num += itemList.Length;
+                num += ItemList.Length;
             }
-            RectTransform RT = Instantiate(itemList[num], contentPanelTransform);
+            RectTransform RT = Instantiate(ItemList[num], contentPanelTransform);
             RT.SetAsFirstSibling();
         }
 
-        contentPanelTransform.localPosition = new Vector3((0 - (itemList[0].rect.width + HLG.spacing) * ItemsToAdd),
-        contentPanelTransform.localPosition.y,
-        contentPanelTransform.localPosition.z);
+        contentPanelTransform.localPosition = new Vector3(contentPanelTransform.localPosition.x,
+            (0 - (ItemList[0].rect.height + VLG.spacing) * ItemsToAdd),
+            contentPanelTransform.localPosition.z);
     }
 
     // Update is called once per frame
@@ -49,18 +51,20 @@ public class InfiniteScroll : MonoBehaviour
             isUpdated = false;
             scrollRect.velocity = OldVelocity;
         }
-        if (contentPanelTransform.localPosition.x > 0)
+
+        if (contentPanelTransform.localPosition.y < 0)
         {
             Canvas.ForceUpdateCanvases();
             OldVelocity = scrollRect.velocity;
-            contentPanelTransform.localPosition -= new Vector3(itemList.Length * (itemList[0].rect.width + HLG.spacing), 0, 0);
+            contentPanelTransform.localPosition += new Vector3(0, ItemList.Length * (ItemList[0].rect.height + VLG.spacing), 0);
             isUpdated = true;
         }
-        if (contentPanelTransform.localPosition.x < 0 - (itemList.Length * (itemList[0].rect.width + HLG.spacing)))
+
+        if (contentPanelTransform.localPosition.y > (ItemList.Length * (ItemList[0].rect.height + VLG.spacing)))
         {
             Canvas.ForceUpdateCanvases();
             OldVelocity = scrollRect.velocity;
-            contentPanelTransform.localPosition += new Vector3(itemList.Length * (itemList[0].rect.width + HLG.spacing), 0, 0);
+            contentPanelTransform.localPosition -= new Vector3(0, ItemList.Length * (ItemList[0].rect.height + VLG.spacing), 0);
             isUpdated = true;
         }
     }
