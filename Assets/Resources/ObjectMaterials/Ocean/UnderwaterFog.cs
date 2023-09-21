47515a8783427ea43932486da25dd2ec370a4665
Max Ekholm <max.m.ekholm@gmail.com>
2023-09-21 12:05:34 +0200

    its pretty pretty

---
+++ Assets/Resources/ObjectMaterials/Ocean/UnderwaterFog.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterFog : MonoBehaviour
+{*
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RenderSettings.fog = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        RenderSettings.fog = false;
+    }
+}
