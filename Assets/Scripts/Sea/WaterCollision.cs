147fe0954ed19c8cae45f7a4d9810eb06d9b2a89
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-09-24 15:40:11 +0200

    Updated waterDrag

--- Assets/Scripts/Sea/WaterCollision.cs
+++ Assets/Scripts/Sea/WaterCollision.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterCollision : MonoBehaviour
 {
     private float waterLevel;
-    private const float FloatHeight = 120f;
-    private readonly float BounceDamp = 0.01f;
+    private const float FloatHeight = 150f;
+    private readonly float BounceDamp = 0.1f;
+    private const float dragMultiplier = 2f;
+    private readonly Dictionary<int, float> addedDragByInstanceID = new();
+    private readonly Dictionary<int, Vector3> previousVelocityByInstanceID = new();
 
     public static event Action OnEnterSea;
 
@@ -20,13 +24,6 @@ public class WaterCollision : MonoBehaviour
         {
             OnEnterSea.Invoke();
         }
-
-        if (other.TryGetComponent<Rigidbody>(out var rigidBody))
-        {
-            rigidBody.drag += 2;
-            // Calculate the magnitude of the impact force
-            float impactForce = rigidBody.velocity.magnitude * rigidBody.mass;
-        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -34,14 +31,34 @@ public class WaterCollision : MonoBehaviour
         if (other.TryGetComponent<Rigidbody>(out var rigidBody))
         {
             ApplyBuoyancy(rigidBody);
+            ApplyDrag(other, rigidBody);
+
         }
+    }
+
+    private void ApplyDrag(Collider other, Rigidbody rigidBody)
+    {
+        int instanceID = other.GetInstanceID();
+        Vector3 currentVelocity = rigidBody.velocity;
+        if(previousVelocityByInstanceID.TryGetValue(instanceID, out Vector3 previousVelocity)) {
+            Vector3 velocityDifference = currentVelocity - previousVelocity;
+            float addedDrag = velocityDifference.magnitude * dragMultiplier;
+            rigidBody.drag = addedDrag;
+            addedDragByInstanceID[instanceID] = addedDrag;
+        }
+       previousVelocityByInstanceID[instanceID] = currentVelocity;
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent<Rigidbody>(out var rigidBody))
         {
-            rigidBody.drag -= 2;
+            if (addedDragByInstanceID.TryGetValue(other.GetInstanceID(), out _))
+            {
+                rigidBody.drag = 0;
+                addedDragByInstanceID.Remove(other.GetInstanceID());
+                previousVelocityByInstanceID.Remove(other.GetInstanceID());
+            }
         }
     }
 
