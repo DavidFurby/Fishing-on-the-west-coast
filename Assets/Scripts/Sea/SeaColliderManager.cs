df12095f828a541e5206c8a51309f0ce665cb0e8
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-09-24 20:50:47 +0200

    refactored seaCollider

--- Assets/Scripts/Sea/SeaColliderManager.cs
+++ Assets/Scripts/Sea/SeaColliderManager.cs
@@ -5,26 +5,28 @@ using System;
 
 public class SeaColliderController : MonoBehaviour
 {
-    private BoxCollider SeaGroupCollider { get; set; }
-    private BoxCollider SeaFloorGroupCollider { get; set; }
+    [SerializeField] private BoxCollider SeaGroupCollider;
+    [SerializeField] private BoxCollider SeaFloorGroupCollider;
 
-
+    public struct BoundsData
+    {
+        public float xMin;
+        public float xMax;
+        public float zMin;
+        public float zMax;
+        public float seaFloorYPosition;
+    }
 
     public void SetBoxCollider(List<GameObject> seaTiles, Vector3 seaSize)
     {
-        if (seaTiles.Count == 0)
-        {
-            return;
-        }
+        if (seaTiles.Count == 0) return;
 
         var seaBounds = CalculateBounds(seaTiles, "SeaFloor");
-        var (xMin, xMax, zMin, zMax, seaFloorYPosition) = CalculateBounds(seaTiles, "SeaFloor");
-
-        SeaGroupCollider = CreateOrUpdateCollider(SeaGroupCollider, (seaBounds.xMin, seaBounds.xMax, seaBounds.zMin, seaBounds.zMax), "SeaGroupCollider", typeof(WaterCollision), seaFloorYPosition / 2, seaSize.y);
-        SeaFloorGroupCollider = CreateOrUpdateCollider(SeaFloorGroupCollider, (xMin, xMax, zMin, zMax), "SeaFloorGroupCollider", typeof(SeaFloorCollision), seaFloorYPosition);
+        SeaGroupCollider = CreateOrUpdateCollider(SeaGroupCollider, seaBounds, "SeaGroupCollider", typeof(WaterCollision), seaBounds.seaFloorYPosition / 2, seaSize.y);
+        SeaFloorGroupCollider = CreateOrUpdateCollider(SeaFloorGroupCollider, seaBounds, "SeaFloorGroupCollider", typeof(SeaFloorCollision), seaBounds.seaFloorYPosition);
     }
 
-    private (float xMin, float xMax, float zMin, float zMax, float seaFloorYPosition) CalculateBounds(List<GameObject> seaTiles, string seaObjectType)
+    private BoundsData CalculateBounds(List<GameObject> seaTiles, string seaObjectType)
     {
         float xMin = Mathf.Infinity;
         float xMax = -Mathf.Infinity;
@@ -34,35 +36,66 @@ public class SeaColliderController : MonoBehaviour
 
         foreach (GameObject seaTile in seaTiles)
         {
-            Transform seaTileObject = seaTile.GetComponentsInChildren<Transform>().First(childTransform => childTransform.CompareTag(seaObjectType));
-            Bounds bounds = seaTileObject.GetComponent<Renderer>().bounds;
-            xMin = Mathf.Min(xMin, bounds.min.x);
-            xMax = Mathf.Max(xMax, bounds.max.x);
-            zMin = Mathf.Min(zMin, bounds.min.z);
-            zMax = Mathf.Max(zMax, bounds.max.z);
+            Transform seaTileObject = FindChildTransformWithTag(seaTile, seaObjectType);
+            Bounds bounds = GetRendererBounds(seaTileObject);
+            UpdateMinMaxValues(ref xMin, ref xMax, ref zMin, ref zMax, bounds);
             seaFloorYPosition = bounds.center.y;
         }
 
-        return (xMin, xMax, zMin, zMax, seaFloorYPosition);
+        return new BoundsData { xMin = xMin, xMax = xMax, zMin = zMin, zMax = zMax, seaFloorYPosition = seaFloorYPosition };
     }
 
-    private BoxCollider CreateOrUpdateCollider(BoxCollider boxCollider, (float xMin, float xMax, float zMin, float zMax) colliderBounds, string colliderName, Type collisionComponentType, float yPosition = 0, float ySize = 0)
+    private Transform FindChildTransformWithTag(GameObject parent, string tag)
     {
-        Vector3 colliderCenter = new((colliderBounds.xMin + colliderBounds.xMax) / 2, yPosition == 0 ? transform.position.y : yPosition, (colliderBounds.zMin + colliderBounds.zMax) / 2);
-        Vector3 colliderSize = new(colliderBounds.xMax - colliderBounds.xMin, ySize, colliderBounds.zMax - colliderBounds.zMin);
+        return parent.GetComponentsInChildren<Transform>().First(childTransform => childTransform.CompareTag(tag));
+    }
+
+    private Bounds GetRendererBounds(Transform transform)
+    {
+        return transform.GetComponent<Renderer>().bounds;
+    }
+
+    private void UpdateMinMaxValues(ref float xMin, ref float xMax, ref float zMin, ref float zMax, Bounds bounds)
+    {
+        xMin = Mathf.Min(xMin, bounds.min.x);
+        xMax = Mathf.Max(xMax, bounds.max.x);
+        zMin = Mathf.Min(zMin, bounds.min.z);
+        zMax = Mathf.Max(zMax, bounds.max.z);
+    }
+
+    private BoxCollider CreateOrUpdateCollider(BoxCollider boxCollider, BoundsData colliderBounds, string colliderName, Type collisionComponentType, float yPosition = 0, float ySize = 0)
+    {
+        Vector3 colliderCenter = CalculateColliderCenter(colliderBounds.xMin, colliderBounds.xMax, yPosition, colliderBounds.zMin, colliderBounds.zMax);
+        Vector3 colliderSize = CalculateColliderSize(colliderBounds.xMin, colliderBounds.xMax, ySize, colliderBounds.zMin, colliderBounds.zMax);
 
         if (boxCollider == null)
         {
-            GameObject colliderObject = new(colliderName);
-            colliderObject.transform.SetParent(transform);
-            boxCollider = colliderObject.AddComponent<BoxCollider>();
-            boxCollider.isTrigger = true;
-            colliderObject.AddComponent(collisionComponentType);
+            boxCollider = CreateNewCollider(colliderName, collisionComponentType);
         }
 
         boxCollider.center = colliderCenter;
         boxCollider.size = colliderSize;
 
         return boxCollider;
+    }
+
+    private Vector3 CalculateColliderCenter(float xMin, float xMax, float yPosition, float zMin, float zMax)
+    {
+        return new Vector3((xMin + xMax) / 2, yPosition == 0 ? transform.position.y : yPosition, (zMin + zMax) / 2);
+    }
+
+    private Vector3 CalculateColliderSize(float xMin, float xMax, float ySize, float zMin, float zMax)
+    {
+        return new Vector3(xMax - xMin, ySize, zMax - zMin);
+    }
+
+    private BoxCollider CreateNewCollider(string colliderName, Type collisionComponentType)
+    {
+        GameObject colliderObject = new(colliderName);
+        colliderObject.transform.SetParent(transform);
+        BoxCollider newBoxCollider = colliderObject.AddComponent<BoxCollider>();
+        newBoxCollider.isTrigger = true;
+        colliderObject.AddComponent(collisionComponentType);
+        return newBoxCollider;
     }
 }
