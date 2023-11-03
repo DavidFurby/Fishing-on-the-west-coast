ce8be4205d9d62175ab98196f0af1b36a99005c3
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-11-03 20:23:30 +0100

    Added method to set balance weights

--- Assets/Scripts/Fishing/FishingMiniGame/Balance.cs
+++ Assets/Scripts/Fishing/FishingMiniGame/Balance.cs
@@ -9,6 +9,7 @@ public class Balance : MonoBehaviour
     private const float DEFAULT_FORCE = 0.0005f;
     private float downwardForce = DEFAULT_FORCE;
     private float upwardForce = DEFAULT_FORCE;
+    private float weight = 0;
 
     void Start()
     {
@@ -37,6 +38,7 @@ public class Balance : MonoBehaviour
     private void SubscribeEvents()
     {
         PlayerEventController.OnEnterReelingFish += StartBalanceMiniGame;
+        PlayerEventController.OnEnterReelingFish += CalculateWeight;
         PlayerEventController.OnWhileReelingFish += MiniGameHandler;
         PlayerEventController.OnExitReelingFish += EndBalanceMiniGame;
     }
@@ -45,6 +47,7 @@ public class Balance : MonoBehaviour
     private void UnsubscribeEvents()
     {
         PlayerEventController.OnEnterReelingFish -= StartBalanceMiniGame;
+        PlayerEventController.OnEnterReelingFish -= CalculateWeight;
         PlayerEventController.OnWhileReelingFish -= MiniGameHandler;
         PlayerEventController.OnExitReelingFish -= EndBalanceMiniGame;
     }
@@ -65,15 +68,19 @@ public class Balance : MonoBehaviour
     {
         if (PlayerController.Instance.fishesOnHook.Count > 0)
         {
-            float weight = 0f;
-            foreach (var fish in PlayerController.Instance.fishesOnHook)
-            {
-                weight += fish.fish.size;
-            }
             float targetValue = Random.value < 0.5 ? reelingBalance.value - Random.Range(0, weight * downwardForce) : reelingBalance.value + Random.Range(0, weight * upwardForce);
 
             targetValue = Mathf.Clamp(targetValue, 0f, 1f);
             reelingBalance.value = Mathf.Lerp(reelingBalance.value, targetValue, Time.deltaTime * 1000f);
+        }
+    }
+
+    private void CalculateWeight()
+    {
+
+        foreach (var fish in PlayerController.Instance.fishesOnHook)
+        {
+            weight += fish.fish.size;
         }
     }
 
@@ -91,67 +98,68 @@ public class Balance : MonoBehaviour
         StartCoroutine(ResetForce());
     }
 
-   // Handle balance color based on value
-   private void HandleBalanceColor()
-   {
-       float colorValue = Mathf.Abs(reelingBalance.value - 0.5f) * 2;
-       reelingBalance.GetComponent<Image>().color = Color.Lerp(Color.blue, Color.red, colorValue);
-   }
+    // Handle balance color based on value
+    private void HandleBalanceColor()
+    {
+        float colorValue = Mathf.Abs(reelingBalance.value - 0.5f) * 2;
+        reelingBalance.GetComponent<Image>().color = Color.Lerp(Color.blue, Color.red, colorValue);
+    }
 
-   // Control balance with user input
-   private void BalanceControls()
-   {
-       if (Input.GetKey(KeyCode.DownArrow))
-       {
-           reelingBalance.value -= 0.005f;
-       }
-       else if (Input.GetKey(KeyCode.UpArrow))
-       {
-           reelingBalance.value += 0.005f;
-       }
-   }
+    // Control balance with user input
+    private void BalanceControls()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            reelingBalance.value -= 0.005f;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            reelingBalance.value += 0.005f;
+        }
+    }
 
-   // Check if balance is lost
-   private void BalanceLost()
-   {
-       if (reelingBalance.value <= 0f || reelingBalance.value >= 1f)
-       {
-           PlayerController.Instance.LoseCatch();
-       }
-   }
+    // Check if balance is lost
+    private void BalanceLost()
+    {
+        if (reelingBalance.value <= 0f || reelingBalance.value >= 1f)
+        {
+            PlayerController.Instance.LoseCatch();
+        }
+    }
 
-   #endregion
+    #endregion
 
-   // Start mini game
-   private void StartBalanceMiniGame()
-   {
-       reelingBalance.gameObject.SetActive(true);
-       InvokeRepeating(nameof(AddForce), 2, 4);
-       musicController.PlayMiniGameMusic();
-   }
+    // Start mini game
+    private void StartBalanceMiniGame()
+    {
+        reelingBalance.gameObject.SetActive(true);
+        InvokeRepeating(nameof(AddForce), 2, 4);
+        musicController.PlayMiniGameMusic();
+    }
 
-   // End mini game
-   private void EndBalanceMiniGame()
-   {
-       if (reelingBalance != null)
-       {
-           reelingBalance.value = 0.5f;
-           reelingBalance.gameObject.SetActive(false);
-           musicController.StopFishingMiniGameMusic();
-       }
-   }
+    // End mini game
+    private void EndBalanceMiniGame()
+    {
+        if (reelingBalance != null)
+        {
+            weight = 0;
+            reelingBalance.value = 0.5f;
+            reelingBalance.gameObject.SetActive(false);
+            musicController.StopFishingMiniGameMusic();
+        }
+    }
 
-   // Reset force after a delay
-   IEnumerator ResetForce()
-   {
-       yield return new WaitForSeconds(1);
-       downwardForce = DEFAULT_FORCE;
-       upwardForce = DEFAULT_FORCE;
-   }
+    // Reset force after a delay
+    IEnumerator ResetForce()
+    {
+        yield return new WaitForSeconds(1);
+        downwardForce = DEFAULT_FORCE;
+        upwardForce = DEFAULT_FORCE;
+    }
 
-   // Get current balance value
-   public float GetBalanceValue()
-   {
-       return reelingBalance.value;
-   }
+    // Get current balance value
+    public float GetBalanceValue()
+    {
+        return reelingBalance.value;
+    }
 }
