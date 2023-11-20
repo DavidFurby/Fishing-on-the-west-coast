eb27628c863919c3b4310430f30fe8aac9960f14
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-11-20 17:34:28 +0100

    Added a rotation for npcs
    Rotate character when conversation starts

--- Assets/Scripts/Interactives/Conversation.cs
+++ Assets/Scripts/Interactives/Conversation.cs
@@ -16,11 +16,19 @@ public class Conversation : MonoBehaviour, IInteractive
         ShopKeeper,
     }
     public static event Action<string> StartConversation;
+    [SerializeField] private PlayerController player;
 
     public void Interact()
     {
         StartConversation.Invoke(character.ToString());
+        RotateTowardsPlayer(player.transform.position);
         PlayerController.Instance.SetState(new Conversing());
     }
 
+    public void RotateTowardsPlayer(Vector3 playerPosition)
+    {
+        Vector3 direction = transform.position - playerPosition;
+        Quaternion playerRotation = Quaternion.LookRotation(direction);
+        transform.rotation = playerRotation;
+    }
 }
