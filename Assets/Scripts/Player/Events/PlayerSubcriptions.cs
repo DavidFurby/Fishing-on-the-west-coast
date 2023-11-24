57d5b1cf74371bd64e65ce1d9e949a203c1e0934
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-11-24 18:18:17 +0100

    Update PlayerSubcriptions.cs

--- Assets/Scripts/Player/Events/PlayerSubcriptions.cs
+++ Assets/Scripts/Player/Events/PlayerSubcriptions.cs
@@ -8,19 +8,27 @@ public class PlayerSubscriptions : MonoBehaviour
     {
         this.manager = manager;
     }
+
     private void Start()
     {
         SubscribeToEvents();
     }
+
     private void OnDestroy()
     {
         UnsubscribeFromEvents();
     }
+
     private void SubscribeToEvents()
     {
+        // WaterCollision events
         WaterCollision.OnBaitEnterSea += manager.fishingController.EnterSea;
+
+        // FishingSpot events
         FishingSpot.StartFishing += (_, _) => manager.SetState(new FishingIdle());
         FishingSpot.StartFishing += manager.SetPlayerPositionAndRotation;
+
+        // PlayerEventController events
         PlayerEventController.OnEnterReelingFish += manager.fishingController.CatchFish;
         PlayerEventController.OnEnterReelingFish += () => manager.SetState(new ReelingFish());
         PlayerEventController.OnEnterIdle += manager.fishingController.ResetValues;
@@ -30,28 +38,45 @@ public class PlayerSubscriptions : MonoBehaviour
         PlayerEventController.OnStartCharging += manager.animations.OnStartCharging;
         PlayerEventController.OnEnterSwinging += manager.animations.OnChargeRelease;
         PlayerEventController.OnEnterIdle += manager.animations.ResetChargingThrowSpeed;
+
+        // CharacterDialog events
         CharacterDialog.OnStartConversation += (_) => manager.SetState(new PlayerInDialog());
+
+        // DialogManager events
         DialogManager.OnEndDialog += manager.ReturnControls;
+
+        // Interactive events
         Interactive.OnEnterInteractive += manager.SetInteractive;
         Interactive.OnExitInteractive += manager.RemoveInteractive;
     }
 
     private void UnsubscribeFromEvents()
     {
+        // WaterCollision events
         WaterCollision.OnBaitEnterSea -= manager.fishingController.EnterSea;
+
+        // FishingSpot events
         FishingSpot.StartFishing -= (_, _) => manager.SetState(new FishingIdle());
         FishingSpot.StartFishing -= manager.SetPlayerPositionAndRotation;
+
+        // PlayerEventController events
         PlayerEventController.OnEnterReelingFish -= manager.fishingController.CatchFish;
         PlayerEventController.OnEnterReelingFish -= () => manager.SetState(new ReelingFish());
         PlayerEventController.OnEnterIdle -= manager.fishingController.ResetValues;
         PlayerEventController.OnWhileCharging -= manager.fishingController.ChargeCasting;
         PlayerEventController.OnWhileCharging -= manager.fishingController.Release;
         PlayerEventController.OnWhileFishing -= manager.fishingController.StartReeling;
-            PlayerEventController.OnStartCharging -= manager.animations.OnStartCharging;
+        PlayerEventController.OnStartCharging -= manager.animations.OnStartCharging;
         PlayerEventController.OnEnterSwinging -= manager.animations.OnChargeRelease;
         PlayerEventController.OnEnterIdle -= manager.animations.ResetChargingThrowSpeed;
+
+        // CharacterDialog events
         CharacterDialog.OnStartConversation -= (_) => manager.SetState(new PlayerInDialog());
+
+        // DialogManager events
         DialogManager.OnEndDialog -= manager.ReturnControls;
+
+        // Interactive events
         Interactive.OnEnterInteractive -= manager.SetInteractive;
         Interactive.OnExitInteractive -= manager.RemoveInteractive;
     }
